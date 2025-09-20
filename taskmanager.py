80c879dce5ebfe4efaec50d06cbd2fde294ec41f
Mohamed2006-Dev <mohamedreda.benomar@etu.uae.ac.ma>
2025-09-20 22:48:18 +0100

    Create TaskManager class

---
+++ taskmanager.py
@@ -0,0 +1,17 @@
+#Task Manager Module
+class TaskManager:
+    def __init__(self):
+        self._tasks = []
+
+    def add_task(task):
+        self._tasks.append(task)
+
+    def remove_task(task):
+        self._tasks.remove(task)
+
+    def get_task(task):
+        return task_name for task_name in self._tasks if task_name == task else -1
+
+    def get_tasks():
+        return self._tasks
+
